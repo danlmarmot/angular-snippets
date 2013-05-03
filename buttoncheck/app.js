64894a26053b6e042c59dc35d88b137a750e1e9b
Daniel McKean <daniel.mckean@neustar.biz>
2013-05-03 14:43:45 -0700

    added filtering of result table by button.

--- buttoncheck/app.js
+++ buttoncheck/app.js
@@ -16,62 +16,69 @@ angular.module('bcApp').factory('configFactory', ['$window', function(win) {
 angular.module('bcApp').controller("buttonCheckCtrl", function checkboxButtons($scope, configFactory) {
     $scope.fruitList = configFactory.getFruitList();
 
+    // initial values
     $scope.buttonVals = { "A":true, "B":true, "C":false };
-
-    //$scope.userFilterRegEx = buildUserFilterRegEx($scope.buttonVals);
+    $scope.showMatchesOnly = false;
 
     $scope.$watch('buttonVals', function() {
         $scope.userFilterRegEx = buildUserFilterRegEx($scope.buttonVals);
+        $scope.fruitsTable = getFruitTable($scope.fruitList, $scope.userFilterRegEx, $scope.showMatchesOnly);
     }, true);
 
-    //build out the table and add sorting
-    $scope.fruitsTable = getFruitTable($scope.fruitList, $scope.buttonVals);
+    $scope.$watch('showMatchesOnly', function() {
+        $scope.fruitsTable = getFruitTable($scope.fruitList, $scope.userFilterRegEx, $scope.showMatchesOnly);
+    });     // do not need true on the end, as showMatchesOnly is not a deep object
+
 });
 
 function buildUserFilterRegEx(b) {
     // Interprets userFilter selections.
     // Note each button is specially handled
+    var r = [];
 
-    var r = "";
+    if (b.A) { r.push("^a.*") }     // starts with 'a'
+    if (b.B) { r.push("^b.*") }
+    if (b.C) { r.push("^c.*") }
+    if (b.D) { r.push("^d.*") }
+    if (b.E) { r.push(".*e$") }     // ends in 'e'
 
-    if (b.A) {
-        r += "A";
+    if (r.length == 0 ) {
+        return "a^";    // regex pattern that matches no strings
     }
-
-    if (b.B) {
-        r += "B";
-    }
-
-    if (b.C) {
-        r += "C";
-    }
-
-    if (b.D) {
-        r += "D";
-    }
-
-    if (b.E) {
-        r += "E";
-    }
-
-    return r;
+        return r.join("|");
 }
 
-function getFruitTable(fruitList, userFilters) {
+function getFruitTable(fruitList, userRegex, matchesOnly) {
     var r = {};
+
+    var patt = new RegExp(userRegex, "gim");
 
     // Build headers
     r.tableHead = [ {display: "Froot", name: "fruit"} ];  // leftmost column
+    r.tableHead.push({display: "RegEx Match?", name: "match"} );
     r.tableHead.push({display: "Quantity", name: "qty"}); // next column
 
     // build tableBody
     r.tableGrid =[];
 
     fruitList.forEach( function(f) {
-        var tableGridRow = {"fruit": f.name};
-        tableGridRow["qty"] = f.qty;
+        var foundMatch = patt.test(f.name);
 
-        r.tableGrid.push(tableGridRow);
+        // exclude if we want matchesOnly and do not have a match
+        if ( !(matchesOnly && !foundMatch)) {
+            var tableGridRow = {"fruit": f.name};
+            tableGridRow["match"] = function () {
+                if (foundMatch) {
+                    return "match: " + f.name.match(patt);
+                } else {
+                    return "no match";
+                }
+            }();
+            //tableGridRow["match"] = userRegex;
+            tableGridRow["qty"] = f.qty;
+
+            r.tableGrid.push(tableGridRow);
+        }
     });
 
     return r;
@@ -79,7 +86,6 @@ function getFruitTable(fruitList, userFilters) {
 
 angular.module('bcApp').directive('buttonCheck', function() {
     return {
-
         require: 'ngModel',
         link : function($scope, element, attr, ctrl) {
 
