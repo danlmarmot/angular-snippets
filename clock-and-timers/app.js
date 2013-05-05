9ecfcb53318ecc4d8a62610bf92aa1e884abcfb7
Dan McKean <danlmarmot@gmail.com>
2013-05-05 14:21:43 -0700

    Adding relative time demo.

--- clock-and-timers/app.js
+++ clock-and-timers/app.js
@@ -6,15 +6,23 @@ angular.module('timerApp').config(function($interpolateProvider) {
 });
 
 angular.module('timerApp').controller("timerCtrl", function ($scope, $timeout) {
+    // Initial values
     $scope.currentTime = "Starting...";
+    $scope.currentEpoch = Math.round(parseInt($scope.currentTime) / 1000);
     $scope.countdownInterval = 10;
     $scope.countdownTicker = $scope.countdownInterval;
     $scope.countdownTriggerCount = 0;
+    $scope.pageLoadDate = Date.now();
+    $scope.pageLoadedTimeAgo = "now";
+    $scope.pageLoadedSecondsAgo = "0";
     
     $timeout(function oncePerSecondTask(){
         $scope.currentTime = Date.now();
         $scope.currentEpoch = Math.round(parseInt($scope.currentTime) / 1000);
         $timeout(oncePerSecondTask, 1000);
+
+        $scope.pageLoadedTimeAgo = moment($scope.pageLoadDate).fromNow();
+        $scope.pageLoadedSecondsAgo++;
     },1000);
 
 
